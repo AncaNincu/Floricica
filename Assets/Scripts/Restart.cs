00dd7e7a65bc1ff09a1afb7e7a9d2df8d71a9392
AncaNincu <anca.nincu@gmail.com>
2020-04-02 19:08:24 +0300

    Finish

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void RestartGame()
+    {
+	ScoreTextScript.coinAmount = 0;
+        SceneManager.LoadScene(0);
+    }
+
+}
